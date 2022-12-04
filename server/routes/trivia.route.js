0d07d4e7cd08b4d94a44e1f091d215935586feaf
mnTalmeeda <michelle.naps@gmail.com>
2022-12-04 09:47:02 -0500

    Update trivia.route.js
    add connection details to trivia.route.js

--- server/routes/trivia.route.js
+++ server/routes/trivia.route.js
@@ -3,3 +3,17 @@
 Trivia question 1: An artist who is top of the billboard and also incorporates multiple genres
 
 */
+
+const config = require('./config.json')
+const mysql = require('mysql');
+const e = require('express');
+
+// Connection details
+const connection = mysql.createConnection({
+    host: "database-550-project.cuttkkiuv1vf.us-east-2.rds.amazonaws.com",
+    port: "3306",
+    user: "admin",
+    password: "450550ansibrmicmua!",
+    URL: "jdbc:mysql://database-550-project.cuttkkiuv1vf.us-east-2.rds.amazonaws.com:3306",
+});
+connection.connect();
